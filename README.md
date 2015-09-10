8c4b160791687412df48c7d5739f5b2513f425cf
Grégoire Audoux <projects@gregoire-audoux.fr>
2015-09-10 23:33:53 +0200

    On normalize l'optimisation des image : npm install for the win. On ajoute un readme pour savoir comment faire

---
+++ README.md
@@ -0,0 +1,8 @@
+# Le site MrFriz
+
+## Pour optiminer les image
+
+    npm install
+    
+ * install tout les modules npm
+ * optimise toute les imgaes presente dans images/
--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var gulp = require('gulp');
 var imagemin = require('gulp-imagemin');
 var pngquant = require('imagemin-pngquant');
- 
+
 gulp.task('default', function () {
     return gulp.src('images/*')
         .pipe(imagemin({
             progressive: true,
+            optimizationLevel: 4,
             svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
         }))
